7b2ca9e8d1fd2f02ecd13682aa2a7ebcfd4529cd
bsbhakti <70367592+bsbhakti@users.noreply.github.com>
2024-09-18 15:49:32 -0700

    PArt 1 done

---
+++ curve_area_parallel.cpp
@@ -0,0 +1,147 @@
+#include "core/utils.h"
+#include <iomanip>
+#include <iostream>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define sqr(x) ((x) * (x))
+#define DEFAULT_NUMBER_OF_POINTS "1000000000"
+#define DEFAULT_A "2"
+#define DEFAULT_B "1"
+#define DEFAULT_RANDOM_SEED "1"
+#define DEFAULT_THREADS "1"
+#define nullptr __nullptr
+
+struct thread_args{
+    unsigned long n_points;
+    uint random_seed;
+    float a;
+    float b;
+    double time_taken;
+    double local_curve_points;
+} ;
+
+
+std::atomic<unsigned long> curve_points;
+uint c_const = (uint)RAND_MAX + (uint)1;
+
+
+void pthread_join(unsigned int n,pthread_t *ptids) {
+  for(int i =0; i < n; i++){
+    pthread_join(ptids[i],nullptr);
+  }
+//   std::cout<<"Joined Thread"<<std::endl;
+}
+
+inline double get_random_coordinate(uint *random_seed) {
+  return ((double)rand_r(random_seed)) / c_const;  // thread-safe random number generator
+}
+
+void * get_points_in_curve(void *arguments) {
+    timer serial_timer;
+    serial_timer.start();
+    thread_args *args = (thread_args *) arguments;
+    unsigned long n = args->n_points;
+    unsigned long curve_count = 0;
+    uint random_seed = args->random_seed;
+    float a = args->a;
+    float b = args->b;
+    
+  
+   double x_coord, y_coord;
+   for (unsigned long i = 0; i < n; i++) {
+    //    std::cout << "I am here inside making point " << i << std::endl;
+       x_coord = ((2.0 * get_random_coordinate(&random_seed)) - 1.0);
+       y_coord = ((2.0 * get_random_coordinate(&random_seed)) - 1.0);
+       if ((a*sqr(x_coord) + b*sqr(sqr(y_coord))) <= 1.0)
+       curve_count++;
+  }
+    curve_points +=  curve_count;
+    args->local_curve_points = curve_count;
+    args->time_taken = serial_timer.stop();
+    return nullptr;
+}
+
+void curve_area_calculation_serial(unsigned long n, float a, float b, uint r_seed, uint n_threads) {
+  timer serial_timer;
+  serial_timer.start();
+  double time_taken = 0;
+  uint random_seed = r_seed;
+  uint each_thread_points = n/n_threads;
+  curve_points = 0;
+  pthread_t *ptids = new pthread_t [n_threads]; 
+  thread_args *all_arguments = new thread_args [n_threads]; 
+
+//   std::cout <<"Each thread will make "<< each_thread_points <<" points"<<std::endl;
+
+
+  for(int i=0; i<n_threads; i++){
+    all_arguments[i].a = a;
+    all_arguments[i].b = b;
+    all_arguments[i].n_points = each_thread_points;
+    all_arguments[i].random_seed = r_seed + i;
+    all_arguments[i].time_taken = 0;
+    all_arguments[i].local_curve_points = 0;
+    // std::cout<<"Making thread "<<i<<std::endl;
+    pthread_create(&ptids[i], NULL, get_points_in_curve, (void*)&all_arguments[i]);
+  }
+
+    pthread_join(n_threads, ptids);
+    // std::cout<<"Done joining all threads "<<std::endl;
+
+//   unsigned long curve_points = get_points_in_curve(n, r_seed, a, b);
+  
+    double area_value = 4.0 * (double)curve_points / (double)n;
+    time_taken = serial_timer.stop();
+
+  //*------------------------------------------------------------------------
+  
+  std::cout << "thread_id, points_generated, curve_points, time_taken\n";
+  for (int i = 0; i < n_threads; i++){
+    std::cout<< i+1 <<", " << each_thread_points << ", " <<std::fixed << std::setprecision(0) << all_arguments[i].local_curve_points
+    << ", " << std::setprecision(TIME_PRECISION) << all_arguments[i].time_taken << "\n";
+  }
+  // std::cout << "1, " << n << ", "
+  //             << curve_points << ", " << std::setprecision(TIME_PRECISION)
+  //             << time_taken << "\n";
+
+  std::cout << "Total points generated : " << n << "\n";
+  std::cout << "Total points in curve : " << curve_points << "\n";
+  std::cout << "Area : " << std::setprecision(VAL_PRECISION) << area_value
+            << "\n";
+  std::cout << "Time taken (in seconds) : " << std::setprecision(TIME_PRECISION)
+            << time_taken << "\n";
+}
+
+int main(int argc, char *argv[]) {
+  // Initialize command line arguments
+  cxxopts::Options options("Curve_area_calculation",
+                           "Calculate area inside curve a x^2 + b y ^4 = 1 using serial and parallel execution");
+  options.add_options(
+      "custom",
+      {
+          {"nPoints", "Number of points",         
+           cxxopts::value<unsigned long>()->default_value(DEFAULT_NUMBER_OF_POINTS)},
+	        {"coeffA", "Coefficient a",
+	         cxxopts::value<float>()->default_value(DEFAULT_A)},
+          {"coeffB", "Coefficient b",
+           cxxopts::value<float>()->default_value(DEFAULT_B)},
+          {"rSeed", "Random Seed",
+           cxxopts::value<uint>()->default_value(DEFAULT_RANDOM_SEED)},
+            {"nThreads", "Number of Threads",
+           cxxopts::value<uint>()->default_value(DEFAULT_THREADS)}
+      });
+  auto cl_options = options.parse(argc, argv);
+  unsigned long n_points = cl_options["nPoints"].as<unsigned long>();
+  float a = cl_options["coeffA"].as<float>();
+  float b = cl_options["coeffB"].as<float>();
+  uint r_seed = cl_options["rSeed"].as<uint>();
+  uint nThreads = cl_options["nThreads"].as<uint>();
+
+  std::cout << "Number of points : " << n_points << "\n";;
+  std::cout << "A : " << a << "\n" << "B : " << b << "\n";
+  std::cout << "Random Seed : " << r_seed << "\n";
+
+  curve_area_calculation_serial(n_points, a, b, r_seed,nThreads );
+  return 0;
+}
---
+++ heat_transfer_parallel.cpp
@@ -0,0 +1,259 @@
+#include "core/utils.h"
+#include <iomanip>
+#include <iostream>
+#include <stdlib.h>
+#include <thread>
+
+
+#define DEFAULT_GRID_SIZE "1000"
+#define DEFAULT_NUMBER_OF_THREADS "1"
+#define DEFAULT_CX "1"
+#define DEFAULT_CY "1"
+#define DEFAULT_TIME_STEPS "1000"
+#define DEFAULT_MIDDLE_TEMP "600"
+#define nullptr __nullptr
+
+
+CustomBarrier *barrier = nullptr;
+
+
+class TemperatureArray {
+private:
+	uint size;
+	uint step;
+	double Cx;
+	double Cy;
+	double *CurrArray;
+	double *PrevArray;
+  void assign(double *A, uint x, uint y, double newvalue) {
+    A[x*size+y] = newvalue;
+  };
+  double read(double *A, uint x, uint y) {
+    return A[x*size+y];
+  }; 
+public:	
+	TemperatureArray(uint input_size, double iCx, double iCy, double init_temp) { // create array of dimension sizexsize
+		size = input_size;
+ 		Cx = iCx;
+    		Cy = iCy;
+		step = 0;
+		CurrArray = (double *)malloc(size*size*sizeof(double));
+		PrevArray = (double *)malloc(size*size*sizeof(double));
+		for (uint i = 0; i < size; i++)
+			for (uint j = 0; j < size; j++) {
+				if ((i > size/3) && (i < 2*size/3) && (j > size/3) && (j < 2*size/3)) {
+					assign(PrevArray, i, j, init_temp); assign(CurrArray, i, j, init_temp);
+				}
+				else {
+					assign(PrevArray, i, j, 0); assign (CurrArray, i, j, 0);
+				}	
+			}
+	};
+ 
+	~TemperatureArray() {
+		free (PrevArray);   free (CurrArray);
+	};
+
+	void IncrementStepCount() { step ++; };
+
+	uint ReadStepCount() { return(step); };
+
+	void ComputeNewTemp(uint x, uint y) {
+		if ((x > 0) && (x < size-1) && (y > 0) && (y < size-1))
+			assign(CurrArray, x, y , read(PrevArray,x,y)	
+				+ Cx * (read(PrevArray, x-1, y) + read(PrevArray, x+1, y) - 2*read(PrevArray, x, y)) 
+				+ Cy * (read(PrevArray, x, y-1) + read(PrevArray, x, y+1) - 2*read(PrevArray, x, y)));
+	};
+
+	void SwapArrays() {
+		double *temp = PrevArray;
+		PrevArray = CurrArray;
+		CurrArray = temp;
+	};	
+ 
+	double temp(uint x, uint y) {
+		return read(CurrArray, x, y);
+	};
+};
+
+struct thread_args {
+    uint tid;
+    uint size;
+    uint start;
+    uint end;
+    double *time_taken;
+    TemperatureArray* T;
+    uint steps;
+};
+
+inline void heat_transfer_calculation(thread_args *all_arguments) {
+//   thread_args *all_arguments = (thread_args *) _all_arguments;
+  std::cout<<"Inside heat_transfer"<<std::endl;
+  uint tid = all_arguments->tid;
+  uint size = all_arguments->size;
+  uint start = all_arguments->start;
+  uint end = all_arguments->end;
+  double *time_taken = all_arguments->time_taken;
+  TemperatureArray* T = all_arguments->T;
+  uint steps = all_arguments->steps;
+  timer t1;
+  t1.start();
+  uint stepcount;
+  for (stepcount = 1; stepcount <= steps; stepcount ++) {
+	  for (uint x = start; x <= end; x++) {
+		  for (uint y = 0; y < size; y++) {
+			  T->ComputeNewTemp(x, y);
+		  }
+	  }
+    //barrier wait
+    std::cout<<"Barrier wait 1: Thread "<< tid<<std::endl;
+    barrier->wait();
+    if (tid == 0) {
+        std::cout<<"Thread 0 is inside"<<std::endl;
+        // thread 0 should swap arrays. This is the only thread in the serial version
+            T->SwapArrays();
+            T->IncrementStepCount();
+        }
+    else {  
+        // other threads should wait until swap is complete
+            std::cout<<"Barrier wait 2, Thread:"<<tid<<std::endl;
+            barrier->wait();
+        }
+    }  // end of current step
+    *time_taken = t1.stop(); //loop ends
+}
+
+void heat_transfer_calculation_serial(uint size, uint number_of_threads, TemperatureArray* T, uint steps) {
+  timer serial_timer;
+  double time_taken = 0.0;
+  std::vector<uint> startx(number_of_threads);
+  std::vector<uint> endx(number_of_threads);
+  std::vector<std::thread> all_threads(number_of_threads);
+//   std::thread *threads = new std::thread [number_of_threads]; 
+  thread_args *all_arguments = new thread_args [number_of_threads]; 
+
+
+
+  // The following code is used to determine start and end of each thread's share of the grid
+  // Also used to determine which points to print out at the end of this function
+  uint min_columns_for_each_thread = size /   number_of_threads;
+  uint excess_columns = size % number_of_threads;
+  uint curr_column = 0;
+
+  for (uint i = 0; i < number_of_threads; i++) {
+    startx[i] = curr_column;
+    if (excess_columns > 0) {
+      endx[i] = curr_column + min_columns_for_each_thread;
+      excess_columns--;
+      } 
+    else {
+           endx[i] = curr_column + min_columns_for_each_thread - 1;
+      }
+    curr_column = endx[i]+1;
+  } 
+  // end of code to determine start and end of each thread's share of the grid
+
+
+  serial_timer.start();
+  //*------------------------------------------------------------------------
+  for(int i=0; i < number_of_threads; i++){
+      all_arguments[i].start = startx[i];
+      all_arguments[i].end = endx[i];
+      all_arguments[i].size = size;
+      all_arguments[i].steps = steps;
+      all_arguments[i].T  = T;
+      all_arguments[i].tid = i;
+      all_arguments[i].time_taken = 0;
+      std::cout<<"Making thread "<<i<<std::endl;
+      std::thread new_thread(heat_transfer_calculation,&(all_arguments[i]));
+      all_threads.push_back(std::move(new_thread));
+    //   pthread_create(&ptids[i], NULL, heat_transfer_calculation, all_arguments[i]));
+    //   heat_transfer_calculation (0, size, startx[0], endx[0], &time_taken, T, steps);
+  }
+  std::cout<<"Total number of threads "<<all_threads.size()<<std::endl;
+
+
+  for(int i=0; i < number_of_threads; i++){
+      std::cout<<"Joining threads"<<std::endl;
+      if(all_threads.at(i).joinable()){
+          all_threads.at(i).join();
+      }
+      
+  }
+    std::cout<<"Done Joining threads"<<std::endl;
+
+
+  // Print these statistics for each thread 
+  std::cout << "thread_id, start_column, end_column, time_taken\n";
+  std::cout << "0, 0, " << size-1 << ", " << std::setprecision(TIME_PRECISION)
+              << time_taken<< "\n";
+  
+  uint step = size/6;
+  uint position = 0;
+  for (uint x = 0; x < 6; x++) {
+      std::cout << "Temp[" << position << "," << position << "]=" << T->temp(position,position) << std::endl;
+      position += step;
+  } 
+  
+  // Print temparature at select boundary points;
+  for (uint i = 0; i < number_of_threads; i++) {
+      std::cout << "Temp[" << endx[i] << "," << endx[i] << "]=" << T->temp(endx[i],endx[i]) << std::endl;
+  }
+
+  //*------------------------------------------------------------------------
+  time_taken = serial_timer.stop();
+
+  std::cout << "Time taken (in seconds) : " << std::setprecision(TIME_PRECISION)
+            << time_taken << "\n";
+}
+
+int main(int argc, char *argv[]) {
+  // Initialize command line arguments
+  cxxopts::Options options("Heat_transfer_calculation",
+                           "Model heat transfer in a grid using serial and parallel execution");
+  options.add_options(
+      "custom",
+      {
+          {"nThreads", "Number of threads",
+           cxxopts::value<uint>()->default_value(DEFAULT_NUMBER_OF_THREADS)},
+          {"gSize", "Grid Size",         
+           cxxopts::value<uint>()->default_value(DEFAULT_GRID_SIZE)},
+          {"mTemp", "Temperature in middle of array",         
+           cxxopts::value<double>()->default_value(DEFAULT_MIDDLE_TEMP)},
+	        {"iCX", "Coefficient of horizontal heat transfer",
+           cxxopts::value<double>()->default_value(DEFAULT_CX)},
+          {"iCY", "Coefficient of vertical heat transfer",
+           cxxopts::value<double>()->default_value(DEFAULT_CY)},
+          {"tSteps", "Time Steps",
+           cxxopts::value<uint>()->default_value(DEFAULT_TIME_STEPS)}
+      });
+  auto cl_options = options.parse(argc, argv);
+  uint n_threads = cl_options["nThreads"].as<uint>();
+//   if (n_threads != 1) {
+// 	std::cout << "Serial version. Number of threads should be equal to 1. Terminating..." << std::endl;
+// 	return 1;
+//   }
+  uint grid_size = cl_options["gSize"].as<uint>();
+  double init_temp = cl_options["mTemp"].as<double>();
+  double Cx = cl_options["iCX"].as<double>();
+  double Cy = cl_options["iCY"].as<double>();
+  uint steps = cl_options["tSteps"].as<uint>();
+  std::cout << "Grid Size : " << grid_size << "x" << grid_size << std::endl;
+  std::cout << "Number of threads : " << n_threads << std::endl;
+  std::cout << "Cx : " << Cx << std::endl << "Cy : " << Cy << std::endl;
+  std::cout << "Temperature in the middle of grid : " << init_temp << std::endl;
+  std::cout << "Time Steps : " << steps << std::endl;
+  
+  std::cout << "Initializing Temperature Array..." << std::endl;
+  TemperatureArray *T = new TemperatureArray(grid_size, Cx, Cy, init_temp);
+  if (!T) {
+      std::cout << "Cannot Initialize Temperature Array...Terminating" << std::endl;
+      return 2;
+  }
+  barrier = new CustomBarrier(n_threads);
+  heat_transfer_calculation_serial (grid_size, n_threads, T, steps);
+
+
+  delete T;
+  return 0;
+}
